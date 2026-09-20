4440b3ecdd8d8087dc3e4fcc8d399f5ff238e5b2
AaronJrovee <iaaronhsiao@gmail.com>
2026-09-20 09:06:57 -0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+# settings.py
+HOST = "snowball-server.onrender.com" # Change to your server's domain or IP when deploying
+PORT = 5555      
+BUFSIZE = 4096
+WIDTH = 800
+HEIGHT = 600
+MAP_SIZE = 2000
+FPS = 60
+START_SIZE = 20
+MAX_PARTICLES = 1000
+RING_SHRINK_RATE = 0.2
+STUN_MULTIPLIER = 10
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+ORANGE = (255, 165, 0)
+BG_COLOR = (200, 240, 255)
+YELLOWISH_WHITE = (255, 255, 220)
